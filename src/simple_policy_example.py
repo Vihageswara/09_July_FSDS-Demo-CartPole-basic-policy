ba5c9796784db176acd66f2885f0a64451353207
c17hawke <sunny.c17hawke@gmail.com>
2022-07-09 18:12:31 +0530

    basic policy done

--- src/simple_policy_example.py
+++ src/simple_policy_example.py
@@ -1,10 +1,45 @@
+from bdb import Breakpoint
 import gym
 import numpy as np
 import time
 
 env = gym.make("CartPole-v1")
 
-Observations = env.reset()
-print(Observations)
-for _ in range(100):
-    env.render()
+# def basic_policy(PoleAngle):
+#     if PoleAngle < 0: # falling left
+#         return 0 # Move left
+#     else: # Move right
+#         return 1 
+def basic_policy(PoleAngle):
+    if PoleAngle < 0: # falling left
+        return 0 # Move left
+    return 1 
+
+total_rewards = list()
+
+N_episodes = 200
+N_steps = 200
+
+for episode in range(N_episodes):
+    rewards = 0
+    # CartPoistion, CartVelocity, PoleAngle, PoleAngularVelocity
+    Observations = env.reset()
+    PoleAngle = Observations[2]
+    for step in range(N_steps):
+        env.render()
+        action = basic_policy(PoleAngle)
+        Observations, reward, done, info = env.step(action)
+        time.sleep(0.001) # sleep
+        rewards += reward
+        if done: # Fallen
+            break
+    total_rewards.append(rewards)
+
+stats = {
+    "mean": np.mean(total_rewards),
+    "std": np.std(total_rewards),
+    "min": np.min(total_rewards),
+    "max": np.max(total_rewards)
+}
+
+print(stats)
